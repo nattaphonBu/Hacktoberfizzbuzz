dda33c363995f3f80b0d14aa7302154917ac990b
unknown <slotsnow0301@gmail.com>
2019-10-29 16:19:55 +0700

    Add Unittest input number 5 should return buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -26,12 +26,23 @@ class FizzbuzzTest(unittest.TestCase):
 
         self.assertEqual(actual, expected)
 
+    def test_input_number_15_should_return_buzz(self):
+        number = 15
+
+        expected = 'buzz'
+        actual = callFizzBuzz(number)
+
+        self.assertEqual(actual, expected)
 
 def callFizzBuzz(number):
+     
+    if number == 15:
+        return 'buzz'
     if number % 3 == 0:
         return 'fizz'
     if number == 5:
         return 'buzz'
+    
 
 unittest.main()
 
